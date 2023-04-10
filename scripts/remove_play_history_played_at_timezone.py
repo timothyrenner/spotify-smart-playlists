568b826fd5b315bcded1632130bc8ff1d56bf576
Tim Renner <renner.timothy@gmail.com>
2023-04-10 07:57:30 -0500

    Better save that repair script cause I will need it again.

---
+++ scripts/remove_play_history_played_at_timezone.py
@@ -0,0 +1,27 @@
+import duckdb
+import typer
+
+
+def main(database_file: str = "spotify.db"):
+    # Strips the timezone from the played_at field in play_history.
+    database = duckdb.connect(database_file)
+    database.execute(
+        """
+    CREATE OR REPLACE TABLE play_history_tz_corrected AS
+    SELECT 
+        track_id,
+        (play_history AT TIME ZONE 'UTC')::TIMESTAMP AS play_history
+    FROM play_history
+    """
+    )
+
+    database.execute(
+        """
+    CREATE OR REPLACE TABLE play_history AS
+    SELECT * FROM play_history_tz_corrected
+    """
+    )
+
+
+if __name__ == "__main__":
+    typer.run(main)
