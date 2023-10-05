7f6639454935ca3ac6dd67fbe363e13a1fd419ec
Tim Renner <renner.timothy@gmail.com>
2023-10-04 19:08:14 -0500

    Switch to local blob storage.

--- pipeline/update_recent_tracks_docker.py
+++ pipeline/update_recent_tracks_docker.py
@@ -3,16 +3,15 @@ from prefect.blocks.system import Secret
 from prefect_gcp import GcpCredentials, GcsBucket
 from update_recent_tracks import update_recent_tracks
 from prefect import flow, get_run_logger
+from prefect.filesystems import RemoteFileSystem
+from pathlib import Path
 
 
 @flow(name="Update recent tracks (Docker)")
 def main(
-    database_file: str = "spotify.db", playlist_config_dir: str = "playlists"
+    data_dir: Path = Path("data"), playlist_config_dir: str = "playlists"
 ):
     logger = get_run_logger()
-
-    logger.info("Fetching credentials.")
-    gcp_credentials = GcpCredentials.load("prefect-gcs-rw")
 
     logger.info("Fetching cache fernet key.")
     cache_fernet_key_block = Secret.load("spotify-cache-fernet-key")
@@ -30,22 +29,35 @@ def main(
     redirect_uri_block = Secret.load("spotify-redirect-uri")
     redirect_uri = redirect_uri_block.get()
 
+    database_file = data_dir.absolute() / "spotify.db"
     logger.info(f"Downloading database to {database_file}")
-    spotify_bucket = GcsBucket(
-        bucket="trenner-datasets", gcp_credentials=gcp_credentials
+    minio_local_dataset_storage = RemoteFileSystem.load(
+        "minio-local-dataset-storage"
     )
-    spotify_bucket.download_object_to_path("spotify/spotify.db", database_file)
+    minio_local_dataset_storage.get_directory(
+        from_path="spotify/data", local_path=str(data_dir)
+    )
 
     logger.info("Executing pipeline.")
     update_recent_tracks(
-        database_file=database_file,
+        database_file=str(database_file),
         cache_fernet_key=cache_fernet_key,
         client_id=client_id,
         client_secret=client_secret,
         redirect_uri=redirect_uri,
     )
 
-    logger.info("Uploading database back to GCS.")
+    logger.info("Saving to remote storage.")
+    minio_local_dataset_storage.put_directory(
+        local_path=str(data_dir), to_path="spotify/data"
+    )
+
+    logger.info("Fetching credentials.")
+    gcp_credentials = GcpCredentials.load("prefect-gcs-rw")
+    logger.info("Uploading database to GCS.")
+    spotify_bucket = GcsBucket(
+        bucket="trenner-datasets", gcp_credentials=gcp_credentials
+    )
     spotify_bucket.upload_from_path(database_file, "spotify/spotify.db")
     logger.info("All done.")
 
--- pipeline/update_smart_playlists_docker.py
+++ pipeline/update_smart_playlists_docker.py
@@ -2,29 +2,17 @@ import typer
 from prefect.blocks.system import Secret
 from prefect_gcp import GcpCredentials, GcsBucket
 from update_smart_playlists import update_smart_playlists
-from prefect import flow, get_run_logger, task
+from prefect import flow, get_run_logger
 from pathlib import Path
-from prefect_shell import ShellOperation
-from prefect.blocks.system import String
-
-
-@task(name="Copy data to local datasets directory")
-def copy_data_to_local_datasets(data_dir: Path, persistent_data_dir: Path):
-    logger = get_run_logger()
-    logger.info(f"Saving contents of {data_dir} to {persistent_data_dir}.")
-    ShellOperation(commands=[f"cp -R {data_dir} {persistent_data_dir}"]).run()
+from prefect.filesystems import RemoteFileSystem
 
 
 @flow(name="Update smart playlists (Docker)")
 def main(
-    database_file: str = "spotify.db",
+    data_dir: Path = "data",
     playlist_config_dir: str = "playlists",
-    exported_data_dir: Path = Path("exported_data"),
 ):
     logger = get_run_logger()
-
-    logger.info("Fetching credentials.")
-    gcp_credentials = GcpCredentials.load("prefect-gcs-rw")
 
     logger.info("Fetching cache fernet key.")
     cache_fernet_key_block = Secret.load("spotify-cache-fernet-key")
@@ -42,29 +30,39 @@ def main(
     redirect_uri_block = Secret.load("spotify-redirect-uri")
     redirect_uri = redirect_uri_block.get()
 
+    database_file = data_dir.absolute() / "spotify.db"
+
     logger.info(f"Downloading database to {database_file}")
-    spotify_bucket = GcsBucket(
-        bucket="trenner-datasets", gcp_credentials=gcp_credentials
+    minio_local_dataset_storage = RemoteFileSystem.load(
+        "minio-local-dataset-storage"
     )
-    spotify_bucket.download_object_to_path("spotify/spotify.db", database_file)
+    minio_local_dataset_storage.get_directory(
+        from_path="spotify/data", local_path=str(data_dir)
+    )
 
     logger.info("Executing pipeline.")
     update_smart_playlists(
-        database_file=database_file,
+        database_file=str(database_file),
         playlist_config_dir=playlist_config_dir,
-        exported_data_dir=exported_data_dir,
+        exported_data_dir=data_dir,
         cache_fernet_key=cache_fernet_key,
         client_id=client_id,
         client_secret=client_secret,
         redirect_uri=redirect_uri,
     )
+    logger.info("Saving to remote storage.")
+    minio_local_dataset_storage.put_directory(
+        local_path=str(data_dir), to_path="spotify/data"
+    )
 
+    logger.info("Fetching GCP credentials.")
+    gcp_credentials = GcpCredentials.load("prefect-gcs-rw")
     logger.info("Uploading database to GCS.")
+    spotify_bucket = GcsBucket(
+        bucket="trenner-datasets", gcp_credentials=gcp_credentials
+    )
     spotify_bucket.upload_from_path(database_file, "spotify/spotify.db")
 
-    logger.info("Fetching local persistent data dir.")
-    persistent_data_dir = String.load("spotify-persistent-data-dir").value
-    copy_data_to_local_datasets(exported_data_dir, persistent_data_dir)
     logger.info("All done.")
 
 
